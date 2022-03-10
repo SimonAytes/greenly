e6ae911ef90edb96ea125c3b096e111d40ea22d3
Eric <ericaragundi@gmail.com>
2022-03-10 02:54:09 -0600

    co unit measures and maps screen fixed

--- map_distance.py
+++ map_distance.py
@@ -3,6 +3,7 @@ from openrouteservice import convert
 import folium
 import json
 
+
 client = openrouteservice.Client(key='5b3ce3597851110001cf624877c925483b25437bb5377f967eccb8d7')
 
 coords = ((-97.7237614,30.4027446),(-97.7517604,30.2659279))
@@ -14,7 +15,7 @@ distance_txt = "<b>Distance " + "<strong>"+str(round(res['routes'][0]['summary']
 duration_txt = "<b>Duration " + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+" Mins. </strong>" +"</b>"
 
 m = folium.Map(location=[30.4027446,-97.7237614],zoom_start=11, control_scale=True)
-folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt,max_width=250)).add_to(m)
+folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt,min_width=15,max_width=40)).add_to(m)
 
 folium.Marker(
     location=list(coords[0][::-1]),
@@ -27,5 +28,37 @@ folium.Marker(
     popup="Austin Central Library Special Events Center",
     icon=folium.Icon(color="green"),
 ).add_to(m)
+
 m
 
+m.save('aloft_to_librery2.html')
+print("DONE!")
+
+
+coords = ((-97.740350,30.274665),(-97.7517604,30.2659279))
+res = client.directions(coords)
+geometry = client.directions(coords)['routes'][0]['geometry']
+decoded = convert.decode_polyline(geometry)
+
+distance_txt2 = "<b>Distance " + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000 * 0.62137,1))+" Miles || </strong>" +"</b>"
+duration_txt2 = "<b>Duration " + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+" Mins. </strong>" +"</b>"
+
+m2 = folium.Map(location=[30.4027446,-97.7237614],zoom_start=11, control_scale=True)
+folium.GeoJson(decoded).add_child(folium.Popup(distance_txt2+duration_txt2,min_width=15,max_width=40)).add_to(m2)
+
+folium.Marker(
+    location=list(coords[0][::-1]),
+    popup="Texas state capitol building",
+    icon=folium.Icon(color="lightgreen"),
+).add_to(m2)
+
+folium.Marker(
+    location=list(coords[1][::-1]),
+    popup="Austin Central Library Special Events Center",
+    icon=folium.Icon(color="green"),
+).add_to(m2)
+
+m2
+
+m2.save('library_to_texas_capitol.html')
+print("DONE!")
--- units_emission.py
+++ units_emission.py
@@ -1,8 +1,9 @@
+
 import pandas as pd
 
 
 carbon = pd.read_csv("~/hsi project/carbon-footprint-travel-mode.csv")
-col_names = ['Transportation Type', 'GHG emissions (gCO2e/km)']
+col_names = ['Transportation Type', 'GHG emissions (gCO2e/km)','Green Score']
 carbon_df = pd.DataFrame(0, index= range(0,3), columns = col_names)
 
 carbon
@@ -13,16 +14,28 @@ for i in range(0,len(carbon),1):
         car_sum.append(carbon['GHG emissions (gCO2e/km)'][i])
         
 print(car_sum)
+car_max = max(car_sum)
         
-carbon_df.loc[0,'Transportation Type'] = "Car" 
-carbon_df.loc[0,'GHG emissions (gCO2e/km)'] = sum(car_sum)/len(car_sum) 
+carbon_df.loc[0,'Transportation Type'] = "Car-average" 
+carbon_df.loc[0,'GHG emissions (gCO2e/km)'] = sum(car_sum)/len(car_sum)
+carbon_df.loc[0,'Green Score'] = 100- ((sum(car_sum)/len(car_sum))/car_max)*100
 
-carbon_df.loc[1,'Transportation Type'] = "Bus" 
-carbon_df.loc[1,'GHG emissions (gCO2e/km)'] = carbon.loc[1,'GHG emissions (gCO2e/km)']
+carbon_df.loc[1,'Transportation Type'] = "Car-max" 
+carbon_df.loc[1,'GHG emissions (gCO2e/km)'] = car_max
+carbon_df.loc[1,'Green Score'] = 0
+
+carbon_df.loc[2,'Transportation Type'] = "Bus" 
+carbon_df.loc[2,'GHG emissions (gCO2e/km)'] = carbon.loc[1,'GHG emissions (gCO2e/km)']
+carbon_df.loc[2,'Green Score'] =100 - (carbon.loc[1,'GHG emissions (gCO2e/km)']/car_max)*100
 
 #Adding scooter info directly to the dataframe
-carbon_df.loc[2,'Transportation Type'] = "Scooter" 
-carbon_df.loc[2,'GHG emissions (gCO2e/km)'] = 202
+carbon_df.loc[3,'Transportation Type'] = "Scooter" 
+carbon_df.loc[3,'GHG emissions (gCO2e/km)'] = 202
+carbon_df.loc[3,'Green Score'] = 100 - (202/car_max)*100
+
+carbon_df.loc[4,'Transportation Type'] = "Car-pool-1person" 
+carbon_df.loc[4,'GHG emissions (gCO2e/km)'] = (sum(car_sum)/len(car_sum))/4
+carbon_df.loc[4,'Green Score'] = 100- (((sum(car_sum)/len(car_sum))/4)/car_max)*100
 
 
 
