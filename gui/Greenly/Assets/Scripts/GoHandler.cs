286a53b4eef630fbd9a9d7d4c814f783aa6248a0
Simon Aytes <simon@aytes.net>
2022-03-10 03:00:45 -0600

    Seed Unity GUI repo

---
+++ gui/Greenly/Assets/Scripts/GoHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoHandler : MonoBehaviour
+{
+    public GameObject ucOne;
+    public GameObject ucTwo;
+    public InputField searchIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize and set both false
+        ucOne.SetActive(false);
+        ucTwo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Use case one 
+        if (searchIn.text == "Austin Public Library")
+        {
+            ucOne.SetActive(false);
+            ucTwo.SetActive(true);
+        }
+        //Use case two
+        else if (searchIn.text == "Aloft Hotel")
+        {
+            ucOne.SetActive(true);
+            ucTwo.SetActive(false);
+        }
+    }
+
+
+
+}
---
+++ gui/Greenly/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class LevelManager : MonoBehaviour
+{
+    public int target_scene = 0;
+    public bool is_splash = false;
+
+
+    void Start()
+    {
+        if (is_splash == true)
+        {
+            Thread.Sleep(500);
+            LoadScene();
+        }
+    }
+    
+    // Start is called before the first frame update
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(target_scene);
+    }
+}
